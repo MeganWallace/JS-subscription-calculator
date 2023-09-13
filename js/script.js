f8ff635143d833efa38add6e1e21ec109b7fe45e
Megan <87830907+MeganWallace@users.noreply.github.com>
2023-09-13 02:19:26 -0400

    JS created change event listeners for subscription and duration dropdowns

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ var subType = "basic"; //default value for subscription dropdown
 var subDuration = 1 //default value for duration dropdown
 
 // Create change event listeners
+  //event listener for subscription dropdown
+subTypeElement.addEventListener("change", function (e) {
+  subType = e.target.value; //the target is the option element which holds the values for the subscription dropdown
+  // console.log(subType);
+});
+
+  // event listener for duration dropdown
+subDurationElement.addEventListener("change", function(e){
+  subDuration = Number(e.target.value);
+  // console.log(subDuration);
+});
