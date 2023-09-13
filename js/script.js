064a689791a032fdae092ae88903e8a1424468b5
Megan <87830907+MeganWallace@users.noreply.github.com>
2023-09-13 01:49:10 -0400

    JS elements selected and variables declared

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-// Your JS code here
+// Select elements and declare variables
+var subTypeElement = document.querySelector("#subscription");
+var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = "basic"; //default value for subscription dropdown
+var subDuration = 1 //default value for duration dropdown
+
+// Create change event listeners
