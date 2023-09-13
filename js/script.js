4c669bd62fb138f371d09b82499d8d75bbfa7d39
Megan <87830907+MeganWallace@users.noreply.github.com>
2023-09-13 02:35:50 -0400

    JS created function to update pricing and duration based on dropdown selections, added function to event listeners

--- js/script.js
+++ js/script.js
@@ -6,14 +6,28 @@ var subType = "basic"; //default value for subscription dropdown
 var subDuration = 1 //default value for duration dropdown
 
 // Create change event listeners
-  //event listener for subscription dropdown
+//event listener for subscription dropdown
 subTypeElement.addEventListener("change", function (e) {
-  subType = e.target.value; //the target is the option element which holds the values for the subscription dropdown
+  subType = e.target.value; //the target is the option element which holds the values for the dropdown
   // console.log(subType);
+  updateSubscriptionDiv(); //tells the browser to update the message each time the selection changes
 });
 
-  // event listener for duration dropdown
-subDurationElement.addEventListener("change", function(e){
+// event listener for duration dropdown
+subDurationElement.addEventListener("change", function (e) {
   subDuration = Number(e.target.value);
   // console.log(subDuration);
+  updateSubscriptionDiv(); //tells the browser to update the message each time the selection changes
 });
+
+// Use a function to calculate the cost
+var updateSubscriptionDiv = function () {
+  var monthlyCost = 5; //for basic plan (default)
+  if (subType === "standard") {
+    monthlyCost = 7;
+  } else if (subType === "premium") {
+    monthlyCost = 10;
+  }
+  var total = subDuration * monthlyCost;
+  result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+};
